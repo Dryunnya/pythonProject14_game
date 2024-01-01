ef3184c9dca04a8b2ae4677874acc480d55f3146
Dryunnya <151667893+Dryunnya@users.noreply.github.com>
2024-01-01 18:25:25 +0300

    картинки для анимации

--- main.py
+++ main.py
@@ -1,16 +1,45 @@
 import os
+import random
 import sys
 import pygame
 import math
+import random
 
 pygame.init()
 screen = pygame.display.set_mode((800, 600))
 clock = pygame.time.Clock()
 
-player_walk_image = [pygame.image.load("walk1.png"), pygame.image.load("walk2.png"), pygame.image.load("walk3.png"),
-                     pygame.image.load("walk4.png"), pygame.image.load("walk5.png"), pygame.image.load("walk6.png"),
-                     pygame.image.load("walk7.png"), pygame.image.load("walk8.png"),
-                     pygame.image.load("walk9.png"), pygame.image.load("walk10.png")]
+player_walk_image = [pygame.image.load("project/walk1.png"), pygame.image.load("project/walk2.png"), pygame.image.load(
+    "project/walk3.png"),
+                     pygame.image.load("project/walk4.png"), pygame.image.load("project/walk5.png"), pygame.image.load(
+        "project/walk6.png"),
+                     pygame.image.load("project/walk7.png"), pygame.image.load("project/walk8.png"),
+                     pygame.image.load("project/walk9.png"), pygame.image.load("project/walk10.png")]
+
+yeti_animation = [pygame.image.load("project/yeti (1).png"), pygame.image.load("project/yeti (2).png"),
+                       pygame.image.load("project/yeti (3).png"), pygame.image.load("project/yeti (4).png"),
+                       pygame.image.load("project/yeti (5).png"), pygame.image.load("project/yeti (6).png"),
+                       pygame.image.load("project/yeti (7).png"), pygame.image.load("project/yeti (8).png"),
+                       pygame.image.load("project/yeti (9).png"), pygame.image.load("project/yeti (10).png"),
+                       pygame.image.load("project/yeti (11).png"), pygame.image.load("project/yeti (12).png"),
+                       pygame.image.load("project/yeti (13).png"), pygame.image.load("project/yeti (14).png"),
+                       pygame.image.load("project/yeti (15).png"), pygame.image.load("project/yeti (16).png"),
+                       pygame.image.load("project/yeti (17).png"), pygame.image.load("project/yeti (18).png"),
+                       pygame.image.load("project/yeti (19).png"), pygame.image.load("project/yeti (20).png"),
+                       pygame.image.load("project/yeti (21).png"), pygame.image.load("project/yeti (22).png"),
+                       pygame.image.load("project/yeti (23).png"), pygame.image.load("project/yeti (24).png"),
+                       pygame.image.load("project/yeti (25).png"), pygame.image.load("project/yeti (26).png"),
+                       pygame.image.load("project/yeti (27).png"), pygame.image.load("project/yeti (28).png"),
+                       pygame.image.load("project/yeti (29).png"), pygame.image.load("project/yeti (30).png"),
+                       pygame.image.load("project/yeti (31).png"), pygame.image.load("project/yeti (32).png"),
+                       pygame.image.load("project/yeti (33).png"), pygame.image.load("project/yeti (34).png"),
+                       pygame.image.load("project/yeti (35).png"), pygame.image.load("project/yeti (36).png"),
+                       pygame.image.load("project/yeti (37).png"), pygame.image.load("project/yeti (38).png"),
+                       pygame.image.load("project/yeti (39).png"), pygame.image.load("project/yeti (40).png"),
+                       pygame.image.load("project/yeti (41).png"), pygame.image.load("project/yeti (42).png"),
+                       pygame.image.load("project/yeti (43).png"), pygame.image.load("project/yeti (44).png"),
+                       pygame.image.load("project/yeti (45).png"), pygame.image.load("project/yeti (46).png"),
+                       pygame.image.load("project/yeti (47).png"), pygame.image.load("project/yeti (48).png")]
 
 
 # class AnimatedSprite(pygame.sprite.Sprite):
@@ -87,6 +116,45 @@ class PlayerBullet:
         pygame.draw.circle(screen, 'yellow', (self.x, self.y), 5)
 
 
+class YetiEnimy:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        self.animation_count = 0
+        self.reset_offset = 0
+        self.offset_x = random.randrange(-300, 300)
+        self.offset_y = random.randrange(-300, 300)
+
+    def main(self, screen):
+        if self.animation_count + 0.2 >= 47:
+            self.animation_count = 0
+        else:
+            self.animation_count += 0.2
+
+        if self.reset_offset == 0:
+            self.offset_x = random.randrange(-300, 300)
+            self.offset_y = random.randrange(-300, 300)
+            self.reset_offset = random.randrange(80, 150)
+        else:
+            self.reset_offset -= 1
+
+        if player.x + self.offset_x > self.x - display_scroll[0]:
+            self.x += 1
+        else:
+            self.x -= 1
+
+        if player.y + self.offset_y > self.y - display_scroll[1]:
+            self.y += 1
+        else:
+            self.y -= 1
+
+        screen.blit(pygame.transform.scale(yeti_animation[int(self.animation_count)], (30, 30)),
+                    (self.x - display_scroll[0], self.y - display_scroll[1]))
+        print(pygame.time.get_ticks())
+
+
+enimies = [YetiEnimy(400, 400)]
+
 player = Player(400, 300, 40, 40)
 
 display_scroll = [0, 0]
@@ -143,6 +211,9 @@ while True:
     for bullet in player_bulets:
         bullet.main(screen)
 
-    clock.tick(60)
+    for model in enimies:
+        model.main(screen)
+
+    clock.tick(40)
 
     pygame.display.update()
